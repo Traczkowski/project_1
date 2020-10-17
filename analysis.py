4c69f74f16d1805d28ee3341d1e96d6cfb866c57
traczkowski <raczkowskitm@gmail.com>
2020-10-17 01:37:52 -0400

    Fixed bug in analysis.py code, handled division by zero with a try/except.

--- analysis.py
+++ analysis.py
@@ -6,7 +6,15 @@ a = 1/2
 #I'm adding this code after the initial commit of this file,
 #Solving for b is very important for our analysis
 
-b = 1/0
+#BUGFIX: Added fix to an error I was getting when I was trying
+#to divide by zero, handled by a try/except for now. Will have
+#to rethink my analysis in the future.
+try:
+    b = 1/0
+except:
+    print("You can't divide by zero!!!")
+    b = 1/100
+
 
 #OLD CODE (WORKS)
 print(a)
