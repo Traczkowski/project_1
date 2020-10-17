a0d0e55f30d0518488eef8a2e6cb0961e8bdd689
traczkowski <raczkowskitm@gmail.com>
2020-10-17 01:19:42 -0400

    Broke analysis.py (I havent figured out how to divide by zero yet!!!)

--- analysis.py
+++ analysis.py
@@ -1,6 +1,15 @@
 #This is the analysis.py code, it will be used for very in-depth analysis.
 
+#OLD CODE (WORKS)
 a = 1/2
 
+#I'm adding this code after the initial commit of this file,
+#Solving for b is very important for our analysis
+
+b = 1/0
+
+#OLD CODE (WORKS)
 print(a)
 
+#Added a new print statement to print b for analytics purposes
+print(b)
